64a106fe488e4a0c63a7dfd3cc49146b24f76cc2
sultan <nnurapins@gmail.com>
2023-09-11 20:09:31 -0400

    updated

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,8 +1,8 @@
 public class Multiples {
     public static void main(String[] args) {
         int count = 0;
-        for (int i = 0; i < 1000; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 count++;
             }
         }
